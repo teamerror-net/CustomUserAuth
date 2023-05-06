a1d9caa6fce08015d981f96d6e29903f1ee6bbc3
teamerror-net <teamerror.net@gmail.com>
2023-05-07 03:00:07 +0600

    Update Model

--- account/models.py
+++ account/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from account.manager import UserManager
 
 # Create your models here.
 
-class UserAuth(AbstractBaseUser,PermissionsMixin):
+
+class UserAuth(AbstractBaseUser, PermissionsMixin):
     class Meta:
-        verbose_name_plural = "User"
-    username = models.CharField(max_length=10,unique=True)
+        verbose_name_plural = "All User"
+
+    username = models.CharField(max_length=10, unique=True)
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     email = models.EmailField(max_length=100)
@@ -16,9 +18,9 @@ class UserAuth(AbstractBaseUser,PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
-    
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["first_name", "last_name", "email"]
 
     objects = UserManager()
 
